9a92140004bcd9df95d3219a246e18021aab6ecb
SuYeon Jo <syjo1227@naver.com>
2022-01-10 22:41:34 +0900

    Initial commit

---
+++ 9498.py
@@ -0,0 +1,17 @@
+K = int(input())
+
+if 100>=K>=90:
+    print("A")
+    
+elif 89>=K>=80:
+        print("B")
+
+elif 79>=K>=70:
+        print("C")
+
+elif 69>=K>=60:
+        print("D")
+
+else:
+        print("F")
+    
